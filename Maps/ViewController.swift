e72255d6314d64a1a628f2dba9aef7dbc68f70d9
Jan Mísař <misar.jan@gmail.com>
2016-04-27 17:37:59 +0200

    Map pins.

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     weak var mapView: MKMapView!
 
@@ -23,11 +23,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization() // nice pod for requesting permissions - https://github.com/nickoneill/PermissionScope
 
         let mapView = MKMapView()
+        mapView.delegate = self
         view.addSubview(mapView)
         mapView.snp_makeConstraints { make in
             make.edges.equalTo(0)
         }
         self.mapView = mapView
+
+        updateAnnotaions()
+    }
+
+    func updateAnnotaions() {
+
+        mapView.removeAnnotations(mapView.annotations)
+
+        locations.forEach { location in
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2DMake(location["lat"]!, location["lon"]!)
+            annotation.title = "Example location"
+            mapView.addAnnotation(annotation)
+        }
     }
 
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
@@ -45,4 +60,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let lastLocation = locations.last! // we are sure we have at least one location there
         print(lastLocation)
     }
+
+    let locations = [
+        ["lat": 50.10155117, "lon": 14.50131164],
+        ["lat": 50.04845155, "lon": 14.40643163],
+        ["lat": 50.01436603, "lon": 14.48202576],
+        ["lat": 50.09773545, "lon": 14.42862526],
+        ["lat": 50.02386574, "lon": 14.418157],
+        ["lat": 50.10891206, "lon": 14.50023615],
+        ["lat": 50.13137991, "lon": 14.43188124],
+        ["lat": 50.03085971, "lon": 14.43437316],
+        ["lat": 50.05523586, "lon": 14.36531867],
+        ["lat": 50.12467219, "lon": 14.39459484],
+        ["lat": 50.00616185, "lon": 14.41959398],
+        ["lat": 50.06693629, "lon": 14.43925259],
+        ["lat": 50.08936261, "lon": 14.53516745],
+        ["lat": 50.03396537, "lon": 14.48803967],
+        ["lat": 50.06252636, "lon": 14.49942098],
+        ["lat": 50.01692711, "lon": 14.37874073],
+        ["lat": 50.07238541, "lon": 14.37937722],
+        ["lat": 50.02807288, "lon": 14.51289626],
+        ["lat": 50.0276592, "lon": 14.48751812],
+        ["lat": 50.1340302, "lon": 14.45877785]
+    ]
 }
