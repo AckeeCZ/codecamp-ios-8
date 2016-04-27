6d61f7949c9172eb3848c3ef588a2b73b67e8161
Jan Mísař <misar.jan@gmail.com>
2016-04-27 17:02:01 +0200

    User location.

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -13,19 +13,20 @@ class ViewController: UIViewController {
 
     weak var mapView: MKMapView!
 
+    let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        locationManager.requestWhenInUseAuthorization()
+
         let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = MKUserTrackingMode.Follow
         view.addSubview(mapView)
         mapView.snp_makeConstraints { make in
             make.edges.equalTo(0)
         }
         self.mapView = mapView
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
